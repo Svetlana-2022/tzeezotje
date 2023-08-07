cd1b2213c15a264cbdd106a22c7ed0d5bf35596e
unknown <cbeta09@list.ru>
2023-08-07 08:29:56 +0500

    refactor: вставила функцию для слайдера

--- script.js
+++ script.js
@@ -14,127 +14,70 @@ const popup = document.querySelector('.popup');
 const buttonClose = popup.querySelector('.popup__button');
 
 // для слайдера
-if (window.innerWidth < 1920) {
-    btnPrev.addEventListener('click', () => {
-        offset = offset - 785; // offset -= 785
-        dotIndex --;
-        if(offset < 0) {
-            offset = 1570;
-            dotIndex = 3;
-        }
-        sliderLine.style.left = - offset + 'px';
-    });
-    btnNext.addEventListener('click', () => {
-        offset = offset + 785; // offset += 785
-        dotIndex ++;
-        if(offset > 1570) {
-            offset = 0;
-            dotIndex = 1;
-        }
-        sliderLine.style.left = - offset + 'px';
-    });
-}
+// if (window.innerWidth < 1920) {
+//     btnPrev.addEventListener('click', () => {
+//         offset = offset - 785; // offset -= 785
+//         dotIndex --;
+//         if(offset < 0) {
+//             offset = 1570;
+//             dotIndex = 3;
+//         }
+//         sliderLine.style.left = - offset + 'px';
+//     });
+//     btnNext.addEventListener('click', () => {
+//         offset = offset + 785; // offset += 785
+//         dotIndex ++;
+//         if(offset > 1570) {
+//             offset = 0;
+//             dotIndex = 1;
+//         }
+//         sliderLine.style.left = - offset + 'px';
+//     });
+// }
+function calcCardsWidth () {
+    const width = { init: 1570, more: 785 };
 
-if (window.innerWidth < 1144) {
-    btnPrev.addEventListener('click', () => {
-        offset = offset - 580;
-        dotIndex --;
-        if(offset < 0) {
-            offset = 1160;
-            dotIndex = 3;
-        }
-        sliderLine.style.left = - offset + 'px';
-    });
-    btnNext.addEventListener('click', () => {
-        offset = offset + 580;
-        dotIndex ++;
-        if(offset > 1160) {
-            offset = 0;
-            dotIndex = 1;
-        }
-        sliderLine.style.left = - offset + 'px';
-    });
-  }
-  if (window.innerWidth < 912) {
-    btnPrev.addEventListener('click', () => {
-        offset = offset - 450;
-        dotIndex --;
-        if(offset < 0) {
-            offset = 900;
-            dotIndex = 3;
-        }
-        sliderLine.style.left = - offset + 'px';
-    });
-    btnNext.addEventListener('click', () => {
-        offset = offset + 450;
-        dotIndex ++;
-        if(offset > 900) {
-            offset = 0;
-            dotIndex = 1;
-        }
-        sliderLine.style.left = - offset + 'px';
-    });
-  }
-  if (window.innerWidth < 760) {
-    btnPrev.addEventListener('click', () => {
-        offset = offset - 320;
-        dotIndex --;
-        if(offset < 0) {
-            offset = 640;
-            dotIndex = 3;
-        }
-        sliderLine.style.left = - offset + 'px';
-    });
-    btnNext.addEventListener('click', () => {
-        offset = offset + 320;
-        dotIndex ++;
-        if(offset > 640) {
-            offset = 0;
-            dotIndex = 1;
-        }
-        sliderLine.style.left = - offset + 'px';
-    });
-  }
-  if (window.innerWidth < 550) {
-    btnPrev.addEventListener('click', () => {
-        offset = offset - 220;
-        dotIndex --;
-        if(offset < 0) {
-            offset = 440;
-            dotIndex = 3;
-        }
-        sliderLine.style.left = - offset + 'px';
-    });
-    btnNext.addEventListener('click', () => {
-        offset = offset + 220;
-        dotIndex ++;
-        if(offset > 440) {
-            offset = 0;
-            dotIndex = 1;
-        }
-        sliderLine.style.left = - offset + 'px';
-    });
-  }
-  if (window.innerWidth < 412) {
-    btnPrev.addEventListener('click', () => {
-        offset = offset - 180;
-        dotIndex --;
-        if(offset < 0) {
-            offset = 360;
-            dotIndex = 3;
-        }
-        sliderLine.style.left = - offset + 'px';
-    });
-    btnNext.addEventListener('click', () => {
-        offset = offset + 180;
-        dotIndex ++;
-        if(offset > 360) {
-            offset = 0;
-            dotIndex = 1;
-        }
-        sliderLine.style.left = - offset + 'px';
-    });
-  }
+    if (window.innerWidth < 1144) {
+        width.init = 1160;
+        width.more = 580;
+    }
+    if (window.innerWidth < 912) {
+        width.init = 900;
+        width.more = 450;
+    }
+    if (window.innerWidth < 760) {
+        width.init = 640;
+        width.more = 320;
+    }
+    if (window.innerWidth < 550) {
+        width.init = 440;
+        width.more = 220;
+    }
+    if (window.innerWidth < 412) {
+        width.init = 360;
+        width.more = 180;
+    }
+    return width;
+}
+const width = calcCardsWidth();
+btnPrev.addEventListener('click', () => {
+    offset = offset - width.more; // offset -= width.more
+    dotIndex --;
+    if(offset < 0) {
+        offset = width.init;
+        dotIndex = 3;
+    }
+    sliderLine.style.left = - offset + 'px';
+});
+btnNext.addEventListener('click', () => {
+    offset = offset + width.more; // width.more
+    dotIndex ++;
+    if(offset > width.init) {
+        offset = 0;
+        dotIndex = 1;
+    }
+    sliderLine.style.left = - offset + 'px';
+});
 
 // для бургера
 burgerOpen.addEventListener('click', () => {
@@ -152,33 +95,33 @@ buttonClose.addEventListener('click', () => {
     popup.classList.remove('popup_opened');
 });
 
-//////////////ajax-запрос
-$("#button").on("click", function(){
-    var nameValue = $("#name").val();
-    console.log(nameValue);
+//////////////ajax-запрос (Не подключилась библиотека: jquery)
+// $("#button").on("click", function(){
+//     var nameValue = $("#name").val();
+//     console.log(nameValue);
 
-    if(nameValue.lenght = 0) {
-        $("#errorMess").text("Введите имя");
-        return;
-    }
-    $("#errorMess").text("");
+//     if(nameValue.lenght = 0) {
+//         $("#errorMess").text("Введите имя");
+//         return;
+//     }
+//     $("#errorMess").text("");
 
-    // отправили данные на сервер (в данном примере some.php)
-	$.ajax({
-		url: './some.php', //адрес сервера
-		method: 'post',
-        cache: false,
-		dataType: 'html',
-		data: { 'name': nameValue },
-        beforeSend: function () {
-            $("#button").prop("disabled", true);
-        },
-        // получили данные с сервера
-        success: function (data) {
-            alert(data);
-            $("#button").prop("disabled", false);
-        }
-	})
-    // очистить инрут
-    // $("#name").val("");
-});
+//     // отправили данные на сервер (в данном примере some.php)
+// 	$.ajax({
+// 		url: './some.php', //адрес сервера
+// 		method: 'post',
+//         cache: false,
+// 		dataType: 'html',
+// 		data: { 'name': nameValue },
+//         beforeSend: function () {
+//             $("#button").prop("disabled", true);
+//         },
+//         // получили данные с сервера
+//         success: function (data) {
+//             alert(data);
+//             $("#button").prop("disabled", false);
+//         }
+// 	})
+//     // очистить инрут
+//     // $("#name").val("");
+// });
